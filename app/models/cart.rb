6171707a1cbdf3c442dbca06454278edc1ac23ae
leandrafinger <leandra.finger@gmail.com>
2014-04-29 15:18:52 +0200

    cart improved

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,8 +4,18 @@ class Cart < ActiveRecord::Base
     def get_total_price
         price = 0
         line_items.each do |line_item|
-            price += line_item.product.price
+            price += line_item.product.price * line_item.quantity
         end
-        return price 
+        return price
+    end
+
+    def add_product(product_id)
+        current_item = line_items.find_by(product_id: product_id)
+        if current_item
+            current_item.quantity += 1
+        else
+            current_item = line_items.build(product_id: product_id)
+        end
+          current_item
     end
 end
