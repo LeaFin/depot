4c3325b0d9f03788e886c7174cd0202f5b2a4597
leandrafinger <leandra.finger@gmail.com>
2014-06-10 11:18:24 +0200

    chapter 11

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
     def number_to_chf(price)
         return sprintf("%0.02f CHF", price)
     end
+    def hidden_div_if(condition, attributes = {}, &block)
+        if condition
+            attributes["style"] = "display: none"
+        end
+        content_tag("div", attributes, &block)
+    end
 end
+
+
